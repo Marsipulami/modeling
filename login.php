e79fecfb4b9b91ea50f22a43034eaff8743f2414
nathan <n.oldersma@kpnwerkplek.com>
2018-07-27 23:08:54 +0200

    test

--- login.php
+++ login.php
@@ -82,15 +82,15 @@ body, html {
 
     <form method="post" action="" >
     <div class="row logonbox">
-        <div class="col-sm-12 col-md-3">
+        <div class=" col-12">
             <div class="row buffer">
-                <div class="col-sm-12 col-md-3"><input type="text" placeholder="Username" name="username" class="form-control input-lg"  autofocus/></div>
+                <div class="col-sm-12 col-md-6"><input type="text" placeholder="Username" name="username" class="form-control input-lg"  autofocus/></div>
             </div>
             <div class="row buffer">
-                <div class=" col-sm-12 col-md-3"><input type="password" placeholder="Password" name="password" class="form-control input-lg" /></div>
+                <div class=" col-sm-12 col-md-6"><input type="password" placeholder="Password" name="password" class="form-control input-lg" /></div>
             </div>
             <div class="row buffer">
-                <div class="col-sm-12 col-md-3"><input type="submit" name="submit"  value="Logon" /></div>
+                <div class="col-sm-12 col-md-6"><input type="submit" name="submit"  value="Logon" /></div>
             </div>
         </div>
     </div>
