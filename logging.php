bbc4f35eaf8e136c6d7272430b12ecc43d21688f
nathan <n.oldersma@kpnwerkplek.com>
2018-07-28 20:04:12 +0200

    layout logging

--- logging.php
+++ logging.php
@@ -24,8 +24,22 @@ session_start();
     $qry = $db_link->prepare("SELECT * FROM logging ORDER By timestamp DESC ");
     $qry->execute();
     $brands = null;
+
+    echo '<div class="container">';
+                    echo '<div class="row top-buffer">';
+                        echo '<div class="col-12">';
+                                    
+
     while($row = $qry->fetch()) {
-        echo $row['timestamp'] .  " " .  $row['logvalue'] . "<br />";
+        echo '<div class="row">';
+            echo '<div class="col-12">';
+                echo $row['timestamp'] .  " " .  $row['logvalue'] . "<br />";
+            echo '</div>';
+        echo '</div>';
     }    
+
+            echo '</div>';
+        echo '</div>';
+    echo '</div>';
 
 ?>
