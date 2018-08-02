3780aa997c9d50ae46296c0bb185e2740eb8370b
nathan <n.oldersma@kpnwerkplek.com>
2018-08-02 18:50:54 +0200

    fix logview

--- logging.php
+++ logging.php
@@ -15,10 +15,21 @@ session_start();
 
   include("includes/menu.inc.php");
 
+
     if($_SESSION['role'] !== "1"){
         die('<div class="alert alert-danger">Onvoldoende rechten.</div>');
     }
+?>
 
+<script>
+        $(document).ready(function () {
+             $('#logtable').DataTable();
+             $('.dataTables_length').addClass('bs-select');
+        });
+
+</script>
+
+<?php
 
 
     $qry = $db_link->prepare("SELECT * FROM logging ORDER By timestamp DESC ");
@@ -30,13 +41,30 @@ session_start();
                         echo '<div class="col-12">';
                                     
 
+    echo '<table id="logtable" class="table table-striped">
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">Message</th>
+       
+        </tr>
+    </thead>
+    <tbody>';
+
+
     while($row = $qry->fetch()) {
-        echo '<div class="row">';
-            echo '<div class="col-12">';
-                echo $row['timestamp'] .  " " .  $row['logvalue'] . "<br />";
-            echo '</div>';
-        echo '</div>';
+
+
+        echo '<tr>
+        <th scope="row">'.$row['timestamp'].'</th>
+        <td>'.$row['logvalue'].'</td>
+        
+            </tr>';
+
     }    
+
+    echo ' </tbody>
+        </table>';
 
             echo '</div>';
         echo '</div>';
