9b1c9e4cf88d943d9fc4930369cbb970350a4e18
nathan <n.oldersma@kpnwerkplek.com>
2018-08-02 13:14:42 +0200

    fix

--- models.php
+++ models.php
@@ -155,10 +155,13 @@ include_once("includes/addPhoto.inc.php");
                     $share = "0";
                 }
 
+                $comments = "";
+                $comments = $_POST['comments'];
+
                 var_dump($_POST);
                 echo "share ".$share;
                 $qry = $db_link->prepare("UPDATE paintdatabase.users_models SET shared= :shared AND comments= :comments WHERE users_models.user_id = :userID AND um_id= :umid");
-                $qry->execute(array(':comments'=>$_POST['comments'], 
+                $qry->execute(array(':comments'=>$comments, 
                                     ':shared'=>$share, 
                                     ':userID'=>$_SESSION['usersid'],
                                     ':umid'=>$_GET['used_model']));
